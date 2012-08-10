209a75dbf1a11352761954f52ecd25dfb3f7398c
Payam Yavari <Payam@Yavari.se>
2012-08-10 22:03:38 +0300

    Got a unit test with search to work

--- app/models/volume.rb
+++ app/models/volume.rb
@@ -1,3 +1,10 @@
 class Volume < ActiveRecord::Base
   attr_accessible :author, :body, :title
+  
+  validates :title, :presence => true
+  
+  searchable do
+    text :title, :author, :body
+  end
+  
 end
--- test/unit/volume_test.rb
+++ test/unit/volume_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class VolumeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save volume without title" do
+    volume = Volume.new
+    assert !volume.save, "Saved the volume without a title"
+  end
+  
+  test "Can search for volume" do
+    volume = Volume.new
+    volume.title = "This is a new volume"
+    volume.body = "This is the new body"
+    !volume.save
+
+    Sunspot.commit
+
+    result = Volume.search do 
+      fulltext 'body'  
+    end
+    assert_equal 1, result.total
+  end
+  
 end
