cf9808c3af32902c502f80d120c4c4e73a387b11
Payam Yavari <Payam@Yavari.se>
2012-08-10 19:21:38 +0300

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchTheWritings::Application.config.secret_token = '6d200d2a29fbb161aa4ed1ec391ac3d1cebcc490ea2ce9d998bfb689068177cc1f5bc3ad4dc8297c6f49f437ac9df55cfa273f13382e06b3515d9b8fd1032e47'
